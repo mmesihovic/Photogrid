c1b248cbd165ac429a606f73f515fd04998b60ee
Rasim <rsabanovic1@gmail.com>
2019-05-15 12:50:46 +0200

    dockerization

--- user-service/src/main/java/nwt/tim14/microservices/user/UserApplication.java
+++ user-service/src/main/java/nwt/tim14/microservices/user/UserApplication.java
@@ -1,7 +1,9 @@
 package nwt.tim14.microservices.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
