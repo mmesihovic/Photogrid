1b47bb6892618bc02097fa334021eeb4464526ac
mmesihovic <mmesihovic1@etf.unsa.ba>
2019-06-02 22:00:54 +0200

    Api Gateway + Other small fixes

--- api-gateway/src/main/java/nwt/tim14/microservices/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/nwt/tim14/microservices/apigateway/ApiGatewayApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@SpringBootApplication
 public class ApiGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
 
 }
